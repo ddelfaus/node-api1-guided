a65a1a754fc8fb36c70e73e0e882ddc82eea45ac
ddelfaus <ddelfaus@gmail.com>
2019-12-02 14:01:05 -0500

    sent data from db

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 // import express from "express"
 //old way
 
-// node way
+// node way to import
 const express = require('express'); // CommonJS Modules
+
+//grabing hubs-model file
+
+const db = require('./data/hubs-model') 
 
 const server = express();
 
 server.get('/', (req, res) => {
     res.send({ api: 'up and running...'})
 })
+
+// list of hubs
+
+server.get('/hubs', (req, res) => {
+    // get the list of hubs from the database
+    db.find()
+    
+    .then(hubs => {
+        res.status(200).json(hubs);
+    })
+    
+    
+    .catch( error => {
+        console.log('error on GET /hubs', error);
+        res.status(500).json({ errorMessage: 'error getting list of hubs from database'})
+    })
+})
+
+
+
+// add a hub
+
+server.post('/hubs', (req, res) => {
+    // get the data the client sent
+})
+
+
+
+// remoove a hub by it's id
+
+// update a hub,
+
+
 
 
 const port = 4000
