189af43eac9d6d4f2e03d2c95bb2742c43ef7da5
ddelfaus <ddelfaus@gmail.com>
2019-12-02 14:22:25 -0500

    can delete from db

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const db = require('./data/hubs-model')
 
 const server = express();
 
+server.use(express.json()); // <<<<<<<<<<<<<<<<<<<< needed to parse json form body
+
 server.get('/', (req, res) => {
     res.send({ api: 'up and running...'})
 })
@@ -37,11 +39,40 @@ server.get('/hubs', (req, res) => {
 
 server.post('/hubs', (req, res) => {
     // get the data the client sent
+    const hubData = req.body; // express does not know how to parse JSON
+
+    db.add(hubData)
+    
+    .then(hub => {
+        res.status(201).json(hub);
+    })
+    .catch(error => {
+        console.log("error on POST /hubs", error);
+        res.status(500).json({errorMessage: "error adding the hub"})
+    })
 })
 
 
 
 // remoove a hub by it's id
+
+server.delete("/hubs/:id", (req, res) => {
+    const id = req.params.id;
+  
+    db.remove(id)
+      .then(removed => {
+        if (removed) {
+          res.status(200).json({ message: "hubs removed successfully", removed });
+        } else {
+          // there was no hub with that id
+          res.status(404).json({ message: "hub not found" });
+        }
+      })
+      .catch(error => {
+        console.log("error on DELET /hubs/:id", error);
+        res.status(500).json({ errorMessage: "error removing the hub" });
+      });
+  });
 
 // update a hub,
 
